638a5158be6c722d08fd4e5802adabad1eb8bf33
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 10:14:47 -0800

    have a good fail for a test for addSequence()
    Co-authored-by Jake Haley <jakechaley@gmail.com>

--- __test__/simon.test.js
+++ __test__/simon.test.js
@@ -9,4 +9,9 @@ describe('Simon', () => {
     expect(newSimon.speed).toEqual(350);
     expect(newSimon.same).toEqual(true);
   });
+  
+  test('should correctly return sequence array with added number and increase number by 1', () => {
+    const newSimon = new Simon();
+    expect(newSimon.addSequence()).toEqual("Number was added to the sequence");
+  });
 });
