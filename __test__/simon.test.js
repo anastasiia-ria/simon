376c5f2f78335592ec70db21c6655cc19939fd61
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-01 13:26:50 -0800

    have a good fail for clearSequences()
    Co-authored-by Jake Haley <jakechaley@gmail.com>

--- __test__/simon.test.js
+++ __test__/simon.test.js
@@ -1,21 +1,20 @@
-import Simon from './../src/simon.js';
+import Simon from "./../src/simon.js";
 
-describe('Simon', () => {
-
-  test('should correctly create a simon object with', () => {
+describe("Simon", () => {
+  test("should correctly create a simon object with", () => {
     const newSimon = new Simon();
     expect(newSimon.number).toEqual(0);
     expect(newSimon.sequence).toEqual([]);
     expect(newSimon.speed).toEqual(350);
     expect(newSimon.same).toEqual(true);
   });
-  
-  test('should correctly return sequence array with added number and increase number by 1', () => {
+
+  test("should correctly return sequence array with added number and increase number by 1", () => {
     const newSimon = new Simon();
     expect(newSimon.addSequence()).toEqual("Number was added to the sequence");
   });
-  
-  test('should correctly determine that sequence entered by user and random sequence are not the same', () => {
+
+  test("should correctly determine that sequence entered by user and random sequence are not the same", () => {
     const newSimon = new Simon();
     newSimon.number = 1;
     newSimon.sequence = [0];
@@ -23,11 +22,17 @@ describe('Simon', () => {
     expect(newSimon.compareSequence(userSequence)).toEqual("They are not the same");
   });
 
-  test('should correctly determine that sequence entered by user and random sequence are the same', () => {
+  test("should correctly determine that sequence entered by user and random sequence are the same", () => {
     const newSimon = new Simon();
     newSimon.number = 1;
     newSimon.sequence = [1];
     const userSequence = [1];
     expect(newSimon.compareSequence(userSequence)).toEqual("They are the same");
+  });
+
+  test("should return empty sequence", () => {
+    const newSimon = new Simon();
+    const userSequence = [1];
+    expect(newSimon.clearSequences(userSequence)).toEqual([]);
   });
 });
